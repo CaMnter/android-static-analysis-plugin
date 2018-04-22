dc0320a4a09659760388026100cc8ba2a0804907
CaMnter <yuanyu.camnter@gmail.com>
2018-04-22 18:49:18 +0800

    [Update] CommandUtils in static-analysis-plugin module

--- static-analysis-plugin/src/main/groovy/com/camnter/android/staticanalysis/plugin/utils/CommandUtils.groovy
+++ static-analysis-plugin/src/main/groovy/com/camnter/android/staticanalysis/plugin/utils/CommandUtils.groovy
@@ -16,6 +16,8 @@
 
 package com.camnter.android.staticanalysis.plugin.utils
 
+import java.util.concurrent.TimeoutException
+
 /**
  * @author CaMnter
  */
@@ -23,59 +25,87 @@ package com.camnter.android.staticanalysis.plugin.utils
 class CommandUtils {
 
     static void command(String command, Closure outputClosure, Closure errorClosure) {
-        PluginUtils.dispatchSystem {
-            commandByOsX(command, outputClosure, errorClosure)
-        } {
-            commandByOsX(command, outputClosure, errorClosure)
-        } {
-            commandByWindow(command, outputClosure, errorClosure)
+        commandByOsX(command, outputClosure, errorClosure)
+    }
+
+    static void mutt(String command) {
+        try {
+            println "[AndroidStaticAnalysisPlugin]   [CommandUtils]   [command] = ${command}"
+            executeCommand(command, 20000)
+        } catch (Exception ignored) {
         }
     }
 
     private static void commandByOsX(String command, Closure outputClosure, Closure errorClosure) {
         try {
-            println "[AndroidStaticAnalysisPlugin]   [CommandUtils]   [command] = ${command}"
             def process = command.execute()
+            println "[AndroidStaticAnalysisPlugin]   [CommandUtils]   [command] = ${command}"
+            printCommandInfo(process, outputClosure, errorClosure)
         } catch (Exception e) {
             errorClosure.call(e.message)
         }
     }
 
-    private static void commandByOsXByBash(String command, Closure outputClosure,
-            Closure errorClosure) {
+    /**
+     * Run an external command to return the status. If the specified timeout is exceeded, a
+     * TimeoutException is thrown
+     * */
+    static int executeCommand(final String command, final long timeout)
+            throws IOException, InterruptedException, TimeoutException {
+        Process process = command.execute()
+        Worker worker = new Worker(process)
+        worker.start()
         try {
-            println "[AndroidStaticAnalysisPlugin]   [CommandUtils]   [command] = ${command}"
-            def process = ['bash', '-c', command].execute()
-        } catch (Exception e) {
-            errorClosure.call(e.message)
+            worker.join(timeout)
+            if (worker.exit != null) {
+                return worker.exit
+            } else {
+                throw new TimeoutException()
+            }
+        } catch (Exception ex) {
+            worker.interrupt()
+            throw ex
+        } finally {
+            process.destroy()
         }
     }
 
-    private static void commandByWindow(String command, Closure outputClosure,
-            Closure errorClosure) {
-        try {
-            println "[AndroidStaticAnalysisPlugin]   [CommandUtils]   [command] = ${command}"
-            def process = ("cmd /c start  /b ${command}").execute()
-        } catch (Exception e) {
-            errorClosure.call(e.message)
+    private static class Worker extends Thread {
+
+        private Integer exit
+        private final Process process
+
+        private Worker(Process process) {
+            this.process = process
+        }
+
+        public void run() {
+            try {
+                exit = process.waitFor()
+            } catch (InterruptedException ignore) {
+                return
+            }
         }
     }
 
     private static void printCommandInfo(Process process, Closure outputClosure,
             Closure errorClosure) {
-        def output = new StringBuilder()
-        def error = new StringBuilder()
-        process.consumeProcessOutput(output, error)
-        process.waitFor()
-        if ('' != output.toString() && 0 != output.length()) {
-            def outputString = output.toString()
-            printf "%6s:  %s", ['output', outputString]
-            if (outputClosure != null) outputClosure.call(outputString)
-        }
-        if ('' != error.toString() && 0 != error.length()) {
-            def errorString = error.toString()
-            printf "%6s:  %s", ['error', errorString]
-            if (errorClosure != null) errorClosure.call(errorString)
+        try {
+            def output = new StringBuilder()
+            def error = new StringBuilder()
+            process.consumeProcessOutput(output, error)
+            process.waitFor()
+            if ('' != output.toString() && 0 != output.length()) {
+                def outputString = output.toString()
+                printf "%6s:  %s", ['output', outputString]
+                if (outputClosure != null) outputClosure.call(outputString)
+            }
+            if ('' != error.toString() && 0 != error.length()) {
+                def errorString = error.toString()
+                printf "%6s:  %s", ['error', errorString]
+                if (errorClosure != null) errorClosure.call(errorString)
+            }
+        } catch (Exception ignored) {
         }
     }
 
